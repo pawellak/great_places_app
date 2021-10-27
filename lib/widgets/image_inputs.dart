fe52e3457b6d6f19ee2f581639761fb2d4a16720
Pawel Lak <pawellak@gmail.com>
2021-10-27 21:42:42 +0200

    Added: image picker and device camera, display the captured image in image inputs widget.

--- lib/widgets/image_inputs.dart
+++ lib/widgets/image_inputs.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInputs extends StatefulWidget {
   const ImageInputs({Key? key}) : super(key: key);
@@ -12,6 +12,18 @@ class ImageInputs extends StatefulWidget {
 class _ImageInputsState extends State<ImageInputs> {
   File? _storedImage;
 
+  Future<void> _takePicture() async {
+    final picker = ImagePicker();
+    final imageFile = await picker.pickImage(
+      source: ImageSource.camera,
+      maxWidth: 600,
+    );
+    setState(() {
+      if(imageFile == null) return;
+      _storedImage = File(imageFile.path);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -22,10 +34,11 @@ class _ImageInputsState extends State<ImageInputs> {
   Expanded buildButton() {
     return Expanded(
         child: TextButton.icon(
-      onPressed: () {},
+      onPressed: _takePicture,
       icon: const Icon(Icons.camera),
       label: const Text('Take picture'),
     ));
+
   }
 
   Container buildImage() {
@@ -35,10 +48,13 @@ class _ImageInputsState extends State<ImageInputs> {
               _storedImage!,
               fit: BoxFit.fill,
             )
-          : const Text('No image Taken',textAlign: TextAlign.center,),
+          : const Text(
+              'No image Taken',
+              textAlign: TextAlign.center,
+            ),
       alignment: Alignment.center,
       height: 100,
-      width: 100,
+      width: 150,
       decoration:
           BoxDecoration(border: Border.all(width: 1, color: Colors.grey)),
     );
