33c95f2ac8c0e5430a5a4df4c9ca3ac27cabac3a
Pawel Lak <pawellak@gmail.com>
2021-10-31 20:42:01 +0100

    Added: fetching user coordinates.

--- lib/widgets/location_inputs.dart
+++ lib/widgets/location_inputs.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 
 class LocationInput extends StatefulWidget {
   const LocationInput({Key? key}) : super(key: key);
@@ -8,7 +9,13 @@ class LocationInput extends StatefulWidget {
 }
 
 class _LocationInputState extends State<LocationInput> {
-   String? _previewImageUrl;
+  String? _previewImageUrl;
+
+  Future<void> _getCurrentLocation() async {
+    final location = await Location().getLocation();
+    print(location.altitude);
+    print(location.longitude);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,20 +23,24 @@ class _LocationInputState extends State<LocationInput> {
       children: [
         Container(
           decoration:
-          BoxDecoration(border: Border.all(width: 1, color: Colors.grey)),
+              BoxDecoration(border: Border.all(width: 1, color: Colors.grey)),
           height: 170,
           width: double.infinity,
           child: _previewImageUrl == null
-              ? const Center(child:  Text('No Location Chosen',textAlign: TextAlign.center,))
+              ? const Center(
+                  child: Text(
+                  'No Location Chosen',
+                  textAlign: TextAlign.center,
+                ))
               : Image.network(_previewImageUrl!,
                   fit: BoxFit.fill, width: double.infinity),
         ),
-        Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-
             TextButton.icon(
               icon: const Icon(Icons.location_on),
-              onPressed: () {},
+              onPressed: _getCurrentLocation,
               label: const Text('Current Location'),
             ),
             TextButton.icon(
